f0783af124a9567fae7ed685dc77df965c2ef7ab
Silvano Costanzo <scostanzo.dev@gmail.com>
2021-06-24 11:23:44 +0200

    setting docker stuff

---
+++ app/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	username = "root"
+	password = "password"
+	hostname = "127.0.0.1:3306"
+	dbname   = "test-db"
+)
+
+func dsn(dbName string) string {
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, hostname, dbName)
+}
+
+func main() {
+	db, err := sql.Open("mysql", dsn(""))
+	if err != nil {
+		log.Printf("Error %s when opening DB\n", err)
+		return
+	}
+	defer db.Close()
+
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	res, err := db.ExecContext(ctx, "CREATE DATABASE IF NOT EXISTS "+dbname)
+	if err != nil {
+		log.Printf("Error %s when creating DB\n", err)
+		return
+	}
+	no, err := res.RowsAffected()
+	if err != nil {
+		log.Printf("Error %s when fetching rows", err)
+		return
+	}
+	log.Printf("rows affected %d\n", no)
+}
